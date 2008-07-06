ad598a889e1f0b0c8147743688be0826937cce54
Marcin Owsiany <marcin@owsiany.pl>
2008-07-06 11:58:17 +0100

    Changed the directory name with spaces to be dynamically created on test data setup.
    We want a directory with spaces in its name to be able to properly test mmgal.
    However perl MakeMaker does not allow for filenames with spaces.

--- testlib/MMGal/TestHelper.pm
+++ testlib/MMGal/TestHelper.pm
@@ -16,6 +16,15 @@ sub prepare_test_data {
 	for my $dir (qw(empty one_dir one_dir/subdir)) {
 		mkdir("td.in/$dir") or die "td.in/$dir: $!" unless -d "td.in/$dir";
 	}
+	# We have to create and populate directories with spaces in their
+	# names, because perl's makemaker does not like them
+	mkdir "td.in/more/zzz another subdir" unless -d "td.in/more/zzz another subdir";
+	my $orig_size = -s "td.in/p.png" or die "Unable to stat td.in/p.png";
+	my $dest_size = -s 'td.in/more/zzz another subdir/p.png';
+	unless ($dest_size and $orig_size == $dest_size) {
+		system('cp', '-a', 'td.in/p.png', 'td.in/more/zzz another subdir/p.png') unless $orig_size == -s 'td.in/more/zzz another subdir/p.png';
+	}
+	# Finally, purge and copy a clean version of the test data into "td"
 	system('rm -rf td ; cp -a td.in td') == 0 or die "Test data preparation failed: $?";
 }
 
