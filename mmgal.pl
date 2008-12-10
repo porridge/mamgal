011f4500c3a0f6fa024b50c29d5a5d0be06e0904
Marcin Owsiany <marcin@owsiany.pl>
2008-12-10 18:10:04 +0000

    Added a handy testing script.

---
+++ mmgal.pl
@@ -0,0 +1,16 @@
+#!/usr/bin/perl
+# mmgal - a program for creating static image galleries
+# Copyright 2007, 2008 Marcin Owsiany <marcin@owsiany.pl>
+# See the README file for license information
+use strict;
+use warnings;
+use lib 'blib/lib';
+
+use MMGal::Maker;
+use MMGal::Formatter;
+use MMGal::MplayerWrapper;
+use MMGal::LocaleEnv;
+my $l = MMGal::LocaleEnv->new;
+$l->set_locale('');
+my $m = MMGal::Maker->new(MMGal::Formatter->new($l), MMGal::MplayerWrapper->new);
+$m->make_roots($ARGV[0]);
