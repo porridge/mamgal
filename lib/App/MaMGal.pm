b1db426620db5428db57d656b0596f45807bfcaa
Marcin Owsiany <marcin@owsiany.pl>
2015-10-05 20:43:46 +0200

    Prepare 1.6

--- lib/App/MaMGal.pm
+++ lib/App/MaMGal.pm
@@ -7,7 +7,7 @@ use strict;
 use warnings;
 use base 'App::MaMGal::Base';
 # Remeber to change po/mamgal.pot as well
-our $VERSION = '1.5';
+our $VERSION = '1.6';
 our $AUTOLOAD;
 use Carp;
 use FileHandle;
